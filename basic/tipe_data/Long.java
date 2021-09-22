f0d66072cd57626ef6b040adb0779753154f0a4a
unknown <afi.firda@gmail.com>
2021-09-22 19:58:30 +0700

    add data types number

---
+++ basic/tipe_data/Long.java
@@ -0,0 +1,19 @@
+// tipe data primitif pada java
+// byte     5
+// short    5
+// int      5
+// long     5
+// float    5.0
+// double   5.0
+// boolean  true / false
+// char     'A'
+
+public class Long{
+    public static void main(String[] args){
+        // long adalah tipe data number yang memiliki value dimulai dari
+        // -9223372036854775808 hingga 9223372036854775807
+        // penggunaan memori pada long paling besar dari pada tipe data number lainnya
+        long angka = 1000000;
+        System.out.println(angka);
+    }
+}
