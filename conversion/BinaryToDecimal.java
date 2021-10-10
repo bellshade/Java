d9b826a7f70a3f898b955da7f6b5f4e89421dc83
slowy07 <slowy.arfy@gmail.com>
2021-10-10 20:53:29 +0700

    feat: menambahkan konversi dari biner ke desimal
    close #32
    
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ conversion/BinaryToDecimal.java
@@ -0,0 +1,24 @@
+// memanggil modul input
+import java.util.Scanner;
+
+public class BinaryToDecimal{
+  public static void main(String[] args){
+    // membuat input
+    Scanner input = new Scanner(System.in);
+    int BinaryNumber, BinCopy, d, s = 0, power = 0;
+    
+    System.out.print("masukkan angka biner: ");
+    BinaryNumber = input.nextInt();
+
+    // copy angka biner ke variabel baru
+    BinCopy = BinaryNumber;
+    
+    while(BinCopy != 0){
+      // BinCopy % 10 / 2
+      d = BinCopy % 10;
+      s += d * (int) Math.pow(2, power++);
+      BinCopy /= 10;
+    }
+    System.out.println("desimalnya: " + s);
+  }
+}
