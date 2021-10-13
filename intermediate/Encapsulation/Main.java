ef3855e2b90adc3991c86faeed7d7345e1803223
sautmanurung1 <68042498+sautmanurung1@users.noreply.github.com>
2021-10-13 11:59:35 +0700

    add: Menambahkan algoritma dari pada Encapsulation (#34)
    * add: Menambahkan algoritma dari pada Encapsulation
    
    * fix: mengubah isi daripada algoritma Encapsulation

---
+++ intermediate/Encapsulation/Main.java
@@ -0,0 +1,17 @@
+package intermediate.Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        // Disini kita mengubah class ContohEncapsulation menjadi object yang dimana
+        // nanti di panggil untuk mengeluarkan hasil dari yang kita buat pada class
+        // ContohEncapsulation
+        ContohEncapsulation object = new ContohEncapsulation();
+        // Disini kita memanggil method GantiBentuk dengan membuat isi sesuai yang kita
+        // buat
+        object.GantiBentuk("Minibus");
+        object.GantiMerk("Isuzu");
+        // Disini kita nge print hasil yang sudah kita buat sebelum nya
+        System.out.println("Bentuk Mobil adalah : " + object.getBentuk());
+        System.out.println("Merk Mobil adalah : " + object.getMerk());
+    }
+}
