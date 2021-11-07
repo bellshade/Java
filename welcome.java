043805948ae783f812fb4c7610ccc77466528a32
Muhammad Dean Fahreza <85747206+random-prog@users.noreply.github.com>
2021-11-07 19:30:50 +0700

    fix: merapihkan folder

--- welcome.java
+++ welcome.java
@@ -1,5 +1,5 @@
 public class welcome{
     public static void main(String[] args){
-        System.out.println("selamat datang di java");
+        System.out.println("selamat datang di Java");
     }
 }
