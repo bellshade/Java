247864331c6fdcc11b7f9c2b6826565e587961a9
slowy07 <slowy07@users.noreply.github.com>
2021-11-14 03:53:35 +0000

    Prettified Code!

--- welcome.java
+++ welcome.java
@@ -1,5 +1,6 @@
 public class welcome {
-    public static void main(String[] args){
-        System.out.println("selamat datang di Java");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("selamat datang di Java");
+  }
 }
