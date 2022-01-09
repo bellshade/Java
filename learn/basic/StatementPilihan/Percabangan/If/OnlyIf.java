4ab078d5d0273c3920c9b9987eb7cc6ef24df94c
Muhammad Dean Fahreza <muhammaddean27@gmail.com>
2022-01-09 20:04:37 +0700

    feat: Menambahkan algoritma baru

---
+++ learn/basic/StatementPilihan/Percabangan/If/OnlyIf.java
@@ -0,0 +1,9 @@
+public class OnlyIf {
+    public static void main(String[] args) {
+        int d = 12;
+
+        if(d >= 12) {
+            System.out.println("Angka inputan sesuai dengan percabangan");
+        }
+    }
+}
