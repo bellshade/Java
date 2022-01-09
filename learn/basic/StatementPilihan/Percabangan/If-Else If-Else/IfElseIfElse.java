2e7a45be8fcd3844588a8174b9b8ada61ed01aef
Muhammad Dean Fahreza <muhammaddean27@gmail.com>
2022-01-09 19:37:58 +0700

    feat: Menambahkan Algoritma baru

---
+++ learn/basic/StatementPilihan/Percabangan/If-Else If-Else/IfElseIfElse.java
@@ -0,0 +1,13 @@
+public class IfElseIfElse {
+    public static void main(String[] args) {
+        int masukan = 220;
+
+        if(masukan % 2 == 0) {
+            System.out.println("Angka yang dimasukkan adalah angka jenis Genap");
+        } else if(masukan % 2 == 1) {
+            System.out.println("Angka yang dimasukkan adalah angka jenis Ganjil");
+        } else {
+            System.out.println("Bukan jenis angka yang dimasukkan");
+        }
+    }
+}
