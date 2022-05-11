06f6fb7a7b91662a41dd4aad8116e99e2bcc1a91
khaira nabila <75076265+khairanabila@users.noreply.github.com>
2022-05-11 10:18:50 +0700

    fix: error package dan fungsi output

--- src/main/java/maths/GCD.java
+++ src/main/java/maths/GCD.java
@@ -1,4 +1,4 @@
-package math;
+package maths;
 
 /*
 * program sederhana untuk menghitung faktor
@@ -50,6 +50,6 @@ public class GCD {
 
         System.out.println(gcd(arraySaya));
 
-        System.out.println("gcd(40,24)=%d\n",gcd(40, 24));
+        System.out.printf("gcd(40,24)=%d\n",gcd(40, 24));
     }
 }
